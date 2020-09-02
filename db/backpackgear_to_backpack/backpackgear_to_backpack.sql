23912123d7310bb8caa899ca2b6f10e52a04aeb5
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-02 15:29:07 -0600

    changed file name

--- db/backpackgear_to_backpack/backpackgear_to_backpack.sql
+++ db/backpackgear_to_backpack/backpackgear_to_backpack.sql
@@ -6,4 +6,4 @@
 SELECT * FROM  backpack_gear
 JOIN backpacks ON backpack_gear.backpack_id = backpacks.backpack_id
 JOIN gear ON backpack_gear.gear_id = gear.gear_id
-WHERE backpack_gear.user_id = $1;
+-- WHERE backpack_gear.user_id = $1;
