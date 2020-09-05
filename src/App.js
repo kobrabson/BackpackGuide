e73030ece55d149415559f0f2a11fe7b52618cfa
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-05 15:38:54 -0600

    routes isn't working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Nav from './components/nav/Nav'
 import './App.css';
 import './reset.css'
+// import Auth from './components/auth/Auth';
+import About from './components/about/About'
 
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      {/* <Auth/> */}
+      <About />
       <h1>WORKING</h1>
     </div>
   );
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,16 @@ import AddResource from './components/addResources/AddResource';
 import Backpack from './components/backpack/Backpack';
 import Gear from './components/gear/Gear';
 import Auth from './components/auth/Auth';
+import about from './components/about/About';
+import Home from './components/Home/Home';
 
 export default (
     <Switch>
-        <Route exact path="/" component={Auth} />
+        <Route exact path="/" component={Home} />
+        <Route path='/about' component={about} />
+        <Route path="/Auth" component={Auth} />
         <Route path="/backpack" component={Backpack} />
-        <Route path="/resource" component={AddResource} />
         <Route path="/gear" component={Gear} />
+        <Route path="/resource" component={AddResource} />
     </Switch>
 )
