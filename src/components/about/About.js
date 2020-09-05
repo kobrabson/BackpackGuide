5d10a81bec1dd542b0d88ad8f3e6d5ab9a368dd8
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-05 15:39:19 -0600

    set up about page

---
+++ src/components/about/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import image from '../image/rockClimbing.JPG'
+
+function About() {
+    return (
+        <div>
+            <h1>About this page</h1>
+    <img className='about image' src={image} alt='rockClimbing' />
+
+
+        </div>
+    )
+}
+
+export default About
