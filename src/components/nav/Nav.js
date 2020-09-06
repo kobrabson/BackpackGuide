18416ca05c61fb862da62afbd523928fc9cf9363
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-06 16:40:45 -0600

    orginized and add links

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Dropdown from './Dropdown';
+import { withRouter, Link } from 'react-router-dom';
 import './Nav.css'
 
 function Nav() {
-
+    
 
     return (
         <div>
@@ -11,15 +12,25 @@ function Nav() {
                 <div className='logo'>
                     {/* logo box */}
                 </div>
-                <div className='title display-ib'>
-                    Backpack Guide
+
+                <div 
+                    className='title display-ib'>
+                        Backpack Guide
                 </div>
+
                 <div className='nav-container'>
-                <button className='login navbar-item'>
-                    Login
-                    {/* change to logout */}
-                </button>
-                <span className='header-item'>about</span>
+
+                <Link to='/Auth'>
+                <button 
+                    className='login navbar-item'>
+                        Login                   
+                </button></Link>
+
+                <Link to='/about'>
+                    <span 
+                        className='header-item'>
+                         about
+                    </span></Link>
                 
                
                 <Dropdown/>
@@ -32,4 +43,4 @@ function Nav() {
     )
 }
 
-export default Nav
+export default withRouter(Nav)
