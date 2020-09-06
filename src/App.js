d784adbfe2871d4092321b2d2f260d78e2a4b8b1
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-06 16:38:44 -0600

    placed routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import Nav from './components/nav/Nav'
 import './App.css';
 import './reset.css'
-// import Auth from './components/auth/Auth';
 import routes from './routes'
-// import About from './components/about/About'
+
 
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-      {/* <Auth/> */}
       {routes}
-      {/* <About /> */}
-      <h1>WORKING</h1>
     </div>
   );
 }
