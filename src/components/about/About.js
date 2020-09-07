ebde997c243eddc8739f8a30365470edf1204cb6
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-07 00:04:10 -0600

    can't get aobut-site pic to right side

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import image from '../image/rockClimbing.JPG'
+import React from 'react';
+import './about.css';
+import image from '../image/rockClimbing.JPG';
 
 function About() {
     return (
-        <div>
-            <h1>About this page</h1>
-    <img className='about image' src={image} alt='rockClimbing' />
-
-
+        <div className='page-container'>
+            <div 
+                className='about-container'>
+            <div
+                className='about-me-container'>
+                 <img 
+                    className='about-image'
+                    src={image}
+                    alt='rockClimbing' />
+            <div className='text-container'>
+                <h1>About me</h1>
+                <p>I'm baby yOLO swag vexillologist, echo park la croix tilde pitchfork master cleanse shabby chic humblebrag man braid actually williamsburg poke. Kogi vinyl literally activated charcoal. Lo-fi typewriter fashion axe neutra, semiotics yuccie franzen pabst vice lumbersexual quinoa. Artisan marfa pok pok chartreuse chia asymmetrical yr, tousled fingerstache seitan keytar. Adaptogen lyft meh, kitsch pinterest jianbing everyday carry you probably haven't heard of them roof party four dollar toast celiac. Shaman raw denim taiyaki enamel pin man bun authentic flannel. Occupy man braid direct trade tbh deep v selfies humblebrag gastropub succulents adaptogen ugh church-key.
+                </p>
+            </div>
         </div>
+        <div className='about-site-container'>
+            <div className='text-2-container'>
+                <h1>About site</h1>
+                <p>I'm baby yOLO swag vexillologist, echo park la croix tilde pitchfork master cleanse shabby chic humblebrag man braid actually williamsburg poke. Kogi vinyl literally activated charcoal. Lo-fi typewriter fashion axe neutra, semiotics yuccie franzen pabst vice lumbersexual quinoa. Artisan marfa pok pok chartreuse chia asymmetrical yr, tousled fingerstache seitan keytar. Adaptogen lyft meh, kitsch pinterest jianbing everyday carry you probably haven't heard of them roof party four dollar toast celiac. Shaman raw denim taiyaki enamel pin man bun authentic flannel. Occupy man braid direct trade tbh deep v selfies humblebrag gastropub succulents adaptogen ugh church-key.
+                </p>
+                <img 
+                    className='about-image-2'
+                    src={image}
+                    alt='rockClimbing' />
+            </div>
+        </div>
+    </div>
+</div>
     )
 }
 
