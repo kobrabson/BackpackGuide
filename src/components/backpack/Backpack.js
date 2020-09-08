653f9d96d15a6afc197fbad5e3e4a67339f547f8
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-08 15:39:24 -0600

    got pack sizes to display

--- src/components/backpack/Backpack.js
+++ src/components/backpack/Backpack.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function Backpack() {
+    const[pack, setPack] = useState([]);
+
+    
+
+    useEffect (() => {
+        axios.get('/api/backpackSize')
+        .then((res) => {
+            setPack(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    },[]);
+
+
+
     return (
         <div>
-            <h1>BACKPACK</h1>
+            <h1>backpacks</h1>
+            <div>
+                {pack.map((packSize, index, array) => {
+                    return (
+                        <div key={index}>
+                            <h1>{ packSize.backpack_weight}</h1>
+                            <h1>{ packSize.backpack_desciption}</h1>
+                        </div>
+                     )
+                })}
+            </div>
+            <div></div>
         </div>
     )
 }
