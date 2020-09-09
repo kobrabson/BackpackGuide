794b31418f66a328cab9b88b8774821211e203db
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-09 14:57:23 -0600

    get single state for backpacks need to implay

--- src/components/backpack/Backpack.js
+++ src/components/backpack/Backpack.js
@@ -1,42 +1,52 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+// ! ----- bootstrap imports ----- !//
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown } from 'react-bootstrap';
+
 function Backpack() {
+	const [ pack, setPack ] = useState([]);
+	const [ selectedPack, setSelectPack ] = useState({});
 
+	useEffect(() => {
+		axios
+			.get('/api/backpackSize')
+			.then((res) => {
+				setPack(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-    
-    const[pack, setPack] = useState([]);
+	return (
+		<div>
+			<h1>backpacks</h1>
+			<div>
+				{pack.map((packSize, index, array) => {
+					return (
+						<div key={index}>
+							<Dropdown>
+								<Dropdown.Toggle className="packSizeDropdown">Choose your pack size</Dropdown.Toggle>
+								<Dropdown.Menu>
+									<Dropdown.Item onClick={() => setSelectPack(packSize)}>
+										<h6>{packSize.backpack_weight}</h6>
+										<h6>{packSize.backpack_desciption}</h6>
+									</Dropdown.Item>
+								</Dropdown.Menu>
+							</Dropdown>
+						</div>
+					);
+				})}
+			</div>
 
-    
-
-    useEffect (() => {
-        axios.get('/api/backpackSize')
-        .then((res) => {
-            setPack(res.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    },[]);
-
-
-
-    return (
-        <div>
-            <h1>backpacks</h1>
-            <div>
-                {pack.map((packSize, index, array) => {
-                    return (
-                        <div key={index}>
-                            <h1>{ packSize.backpack_weight}</h1>
-                            <h1>{ packSize.backpack_desciption}</h1>
-                        </div>
-                     )
-                })}
-            </div>
-            <div></div>
-        </div>
-    )
+			<div>
+				<h1>Selected pack size</h1>
+			</div>
+		</div>
+	);
 }
 
-export default Backpack
+export default Backpack;
