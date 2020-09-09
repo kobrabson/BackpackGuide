321afbff2b22252c114b4797bb8e1116ae8010ec
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-09 13:29:59 -0600

    GOT BOOTSTRAP TO WORK WHOOT WHOOT

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Home.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Dropdown } from 'react-bootstrap';
 
 function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+	return (
+		<div>
+			<h1>HOME PAGE</h1>
+			<Dropdown>
+				<Dropdown.Toggle>Dropdown</Dropdown.Toggle>
+				<Dropdown.Menu>
+					<Dropdown.Item>helle</Dropdown.Item>
+					<Dropdown.Item>world</Dropdown.Item>
+				</Dropdown.Menu>
+			</Dropdown>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
