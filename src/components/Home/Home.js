1f4b53ab0afebe6089c332a9c3fcbe24d93e2e6e
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-09 14:56:29 -0600

    get rid of example bootstrap

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import './Home.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import { Dropdown } from 'react-bootstrap';
 
 function Home() {
 	return (
 		<div>
 			<h1>HOME PAGE</h1>
-			<Dropdown>
-				<Dropdown.Toggle>Dropdown</Dropdown.Toggle>
-				<Dropdown.Menu>
-					<Dropdown.Item>helle</Dropdown.Item>
-					<Dropdown.Item>world</Dropdown.Item>
-				</Dropdown.Menu>
-			</Dropdown>
 		</div>
 	);
 }
