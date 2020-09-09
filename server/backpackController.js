f5538f260387a54adf1e49ed311667d7ea2bbe00
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-09 14:55:36 -0600

    desited not to do get by id

--- server/backpackController.js
+++ server/backpackController.js
@@ -1,20 +1,18 @@
-
 module.exports = {
-    getBackpackSize: async (req, res) => {
-        const db = req.app.get("db");
-        const backpacks = await db.backpack_to_user.get_backpack_sizes();
-        res.status(200).send(backpacks)
-    },
+	getBackpackSize: async (req, res) => {
+		const db = req.app.get('db');
+		const backpacks = await db.backpack_to_user.get_backpack_sizes();
+		res.status(200).send(backpacks);
+	},
 
-    getPack: async (req, res) => {
-        const db = req.app.get("db");
-        const backpack_gear = await db.backpackgear_to_backpack.backpackgear_to_backpack();
-        res.status(200).send(backpack_gear)
-    },
+	getPack: async (req, res) => {
+		const db = req.app.get('db');
+		const backpack_gear = await db.backpackgear_to_backpack.backpackgear_to_backpack();
+		res.status(200).send(backpack_gear);
+	}
 
-    // addToPack: async (req, res) => {
-    //     const db = req.app.get("db");
+	// addToPack: async (req, res) => {
+	//     const db = req.app.get("db");
 
-    // }
-
-}
+	// }
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
-require("dotenv").config();
-express = require("express"),
-massive = require("massive"),
-session = require("express-session");
+require('dotenv').config();
+(express = require('express')), (massive = require('massive')), (session = require('express-session'));
 
-const authCtrl = require("./authController");
-const packCtrl = require("./backpackController");
-const gearCtrl = require("./gearController");
+const authCtrl = require('./authController');
+const packCtrl = require('./backpackController');
+const gearCtrl = require('./gearController');
 
 const app = express();
 
@@ -14,58 +12,45 @@ app.use(express.json());
 const { SESSION_SECRET, CONNECTION_STRING, SERVER_PORT } = process.env;
 
 app.use(
-    session({
-      resave: false,
-      saveUninitialized: true,
-      secret: SESSION_SECRET,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 30,
-      },
-    })
-  );
+	session({
+		resave: false,
+		saveUninitialized: true,
+		secret: SESSION_SECRET,
+		cookie: {
+			maxAge: 1000 * 60 * 60 * 24 * 30
+		}
+	})
+);
 
-  massive({
-    connectionString: CONNECTION_STRING,
-    ssl: { rejectUnauthorized: false },
-  })
-    .then((db) => {
-      app.set("db", db);
-      console.log("<-----Database Online----->");
-    })
-    .catch((err) => console.log(`Database error: ${err}`));
-
-
+massive({
+	connectionString: CONNECTION_STRING,
+	ssl: { rejectUnauthorized: false }
+})
+	.then((db) => {
+		app.set('db', db);
+		console.log('<-----Database Online----->');
+	})
+	.catch((err) => console.log(`Database error: ${err}`));
 
 //  AUTH END POINTS
-    app.post("/auth/register", authCtrl.register);
-    app.post("/auth/login", authCtrl.login);
-    app.post("/auth/logout", authCtrl.logout);
-    app.get("/auth/user", authCtrl.getUser);
+app.post('/auth/register', authCtrl.register);
+app.post('/auth/login', authCtrl.login);
+app.post('/auth/logout', authCtrl.logout);
+app.get('/auth/user', authCtrl.getUser);
 
-    // GEAR END POINTS
-    app.get("/api/gear", gearCtrl.getAllGear);
-    app.post("/api/gear", gearCtrl.addGear);
-    app.delete("/api/gear/:id", gearCtrl.deleteGear);
+// GEAR END POINTS
+app.get('/api/gear', gearCtrl.getAllGear);
+app.post('/api/gear', gearCtrl.addGear);
+app.delete('/api/gear/:id', gearCtrl.deleteGear);
 
-    // I MAY NOT USE PUT METHOD 
-    // app.put("/api/gear", gearCtrl.editGear)
+// I MAY NOT USE PUT METHOD
+// app.put("/api/gear", gearCtrl.editGear)
 
-    // BACKPACK SIZE END POINT
-    app.get("/api/backpackSize", packCtrl.getBackpackSize);
+// BACKPACK SIZE END POINT
+app.get('/api/backpackSize', packCtrl.getBackpackSize);
 
-    // PACK END POINT
-    app.get("/api/pack", packCtrl.getPack);
-    // app.post("/api/pack", packCtrl.addToPack);
-    
-    
+// PACK END POINT
+app.get('/api/pack', packCtrl.getPack);
+// app.post("/api/pack", packCtrl.addToPack);
 
-
-
-
-
-
-
-
-
-
-    app.listen(SERVER_PORT, () => console.log(`<-----Server Online port ${SERVER_PORT}----->`));
+app.listen(SERVER_PORT, () => console.log(`<-----Server Online port ${SERVER_PORT}----->`));
