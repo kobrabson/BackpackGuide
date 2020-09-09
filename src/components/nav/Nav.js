ff5e71c729280d08e1e9c7c4c48e79fe82c2cbd6
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-09 14:58:44 -0600

    got LI and LO button working

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,70 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Dropdown from './Dropdown';
-import { logoutUser } from '../../redux/reducer'
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux'
-import './Nav.css'
-// import Axios from 'axios';
+import { logoutUser } from '../../redux/reducer';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import './Nav.css';
+import Axios from 'axios';
 
 const Nav = (props) => {
+	const [ isLoggedIn, setIsLoggedIn ] = useState(false);
 
-    // const [isLoggedIn, setIsLoggedIn] = useState(false)
+	const logout = () => {
+		Axios.post('/auth/logout')
+			.then((res) => {
+				setIsLoggedIn(!isLoggedIn);
+				props.logoutUser();
+				props.history.push('/');
+			})
+			.catch((err) => console.log(err));
+	};
 
-    // const logout = () => {
-    //     Axios.get('/auth/logout')
-    //     .then((res) => {
-    //         props.logoutUser()
-    //         props.history.push('/')
-    //     })
-    //     .catch((err) => console.log(err));
-    // }
-    
+	return (
+		<div className="nav-bar">
+			<nav className="navbar display-ib">
+				<div className="logo">{/* logo box */}</div>
 
-    return (
-        <div className='nav-bar'>
-            <nav className='navbar display-ib'>
-                <div className='logo'>
-                    {/* logo box */}
-                </div>
+				<div className="title display-ib">Backpack Guide</div>
 
-                <div 
-                    className='title display-ib'>
-                        Backpack Guide
-                </div>
+				<div className="nav-container">
+					{!isLoggedIn ? (
+						<Link to="/Auth">
+							<button className="login navbar-item" onClick={() => setIsLoggedIn(!isLoggedIn)}>
+								Login
+							</button>
+						</Link>
+					) : (
+						<button className="login navbar-item" onClick={() => logout()}>
+							Logout
+						</button>
+					)}
 
-                <div className='nav-container'>
-{/* 
-                    {isLoggedIn ? false (
-                        <Link to='/Auth'>
-                            <button 
-                                className='login navbar-item'
-                                onClick={() => setIsLoggedIn(!isLoggedIn)}
-                                        > Login </button></Link>
-                     ) : true (
-                         <button 
-                         className='login navbar-item'
-                         onClick={() => logout}
-                        > Logout </button>
-                     )} */}
+					<Link className="links" to="/about">
+						<span className="header-item about">about</span>
+					</Link>
 
+					<Dropdown />
+				</div>
+			</nav>
+		</div>
+	);
+};
 
-                <Link className='links' to='/about'>
-                    <span 
-                        className='header-item about'>
-                         about
-                    </span></Link>
-                
-               
-                <Dropdown/>
+const mapStateToProps = (state) => state;
 
-                </div>
-                
-            </nav>
-            
-        </div>
-    )
-}
-
-const mapStateToProps = state => state;
-
-export default connect(mapStateToProps, {logoutUser})(Nav)
+export default withRouter(connect(mapStateToProps, { logoutUser })(Nav));
