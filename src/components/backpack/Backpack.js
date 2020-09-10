321ecd2743f5edbaf64e1c6c048f2fde8dda41a7
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-10 16:32:25 -0600

    made card for gear

--- src/components/backpack/Backpack.js
+++ src/components/backpack/Backpack.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { selectPack } from '../../redux/reducer';
+import './Backpack.css';
 
 // ! ----- bootstrap imports ----- !//
 
@@ -23,17 +24,20 @@ function Backpack(props) {
 			});
 	}, []);
 
-	useEffect(() => {
-		axios
-			.get(`/api/pack/${props.reducer.user.user_id}`)
-			.then((res) => {
-				setMyGear(res.data);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
-	// console.log(props.reducer.selectedPack);
+	useEffect(
+		() => {
+			axios
+				.get(`/api/pack/${props.reducer.user.user_id}`)
+				.then((res) => {
+					setMyGear(res.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+		[ props.reducer.user.user_id ]
+	);
+	console.log(props.reducer.user.user_id);
 	return (
 		<div>
 			<h1>backpacks</h1>
@@ -41,16 +45,6 @@ function Backpack(props) {
 				{pack.map((packSize, index, array) => {
 					return (
 						<div className="d-inline-flex justify-content-center flow-row" key={index}>
-							{/* <Dropdown>
-								<Dropdown.Toggle className="packSizeDropdown">Choose your pack size</Dropdown.Toggle>
-								<Dropdown.Menu>
-									<Dropdown.Item onClick={() => props.selectPack(packSize)}>
-										<h6>{packSize.backpack_weight}</h6>
-										<h6>{packSize.backpack_desciption}</h6>
-									</Dropdown.Item>
-								</Dropdown.Menu>
-							</Dropdown> */}
-
 							<div className="btn-toolbar d-inline-flex justify-content-center flow-row" role="toolbar">
 								<button
 									onClick={() => props.selectPack(packSize)}
@@ -65,9 +59,27 @@ function Backpack(props) {
 				})}
 			</div>
 
-			<div>
+			<div className="">
 				<h1>Your Pack</h1>
 				<h2>{props.reducer.selectedPack.backpack_weight}</h2>
+				<div className="d-inline-block flex-row m-2">
+					{myGear.map((myGear, i, array) => {
+						return (
+							<div className="card d-inline-block flex-row m-2" key={i}>
+								<img
+									className="my-gear-image card-img-top p-3"
+									key={i}
+									src={myGear.image}
+									alt={myGear.image}
+								/>
+								<h5 className="card-body">
+									{myGear.name} {myGear.weight}
+									{myGear.unit}
+								</h5>
+							</div>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
