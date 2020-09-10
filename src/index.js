187cc7f1d3f9adbb5b7c83cb687764d0a8c35424
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-10 10:07:01 -0600

    WARNING HERE FIX BUG BUT BE CAREFUL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter} from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
@@ -8,14 +8,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.Fragment>
+		<Provider store={store}>
+			<HashRouter>
+				<App />
+			</HashRouter>
+		</Provider>
+	</React.Fragment>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
