93c04501156d54de5268fdfd029bfd31cf5e9963
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-11 16:08:13 -0600

    spacing

--- src/components/nav/Dropdown.js
+++ src/components/nav/Dropdown.js
@@ -1,36 +1,43 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import './Nav.css';
 
 function Dropdown() {
-    const [open, setOpen] = useState(false);
-    const toggle = () => setOpen(!open);
+	const [ open, setOpen ] = useState(false);
+	const toggle = () => setOpen(!open);
 
-
-    return (
-        <div
-        className="display-ib"
-        tabIndex={0}
-        role="button"
-        onKeyPress={()=> toggle(!open)}
-        onClick={()=> toggle(!open)}
-        >
-            
-            <div className='nav-action navbar-item' >
-                <p className='open-menu'>{open ? 'Close' : 'Open' }</p>
-            </div>
-            {open && (
-                <div className='nav-list-item'>
-                    <Link to='/about' className='nav-item links'>About</Link>
-                    <Link className='links' to='/gear'><ul className='nav-item'>Gear</ul></Link>
-                    <Link className='links' to='/backpack'><ul className='nav-item'>Backpack</ul></Link>
-                    <Link className='links' to='/resource'><ul className='nav-item'>Addition Resources</ul></Link>
-                    <Link className='links' to='/Auth'><ul className='nav-item'>Register</ul></Link>
-                </div>
-            
-            )}
-        </div>
-    )
+	return (
+		<div
+			className="display-ib"
+			tabIndex={0}
+			role="button"
+			onKeyPress={() => toggle(!open)}
+			onClick={() => toggle(!open)}
+		>
+			<div className="nav-action navbar-item">
+				<p className="open-menu">{open ? 'Close' : 'Open'}</p>
+			</div>
+			{open && (
+				<div className="nav-list-item">
+					<Link to="/about" className="nav-item links">
+						About
+					</Link>
+					<Link className="links" to="/gear">
+						<ul className="nav-item">Gear</ul>
+					</Link>
+					<Link className="links" to="/backpack">
+						<ul className="nav-item">Backpack</ul>
+					</Link>
+					<Link className="links" to="/resource">
+						<ul className="nav-item">Addition Resources</ul>
+					</Link>
+					<Link className="links" to="/Auth">
+						<ul className="nav-item">Register</ul>
+					</Link>
+				</div>
+			)}
+		</div>
+	);
 }
 
-export default withRouter(Dropdown)
+export default withRouter(Dropdown);
