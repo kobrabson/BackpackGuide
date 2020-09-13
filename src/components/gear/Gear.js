71d1a730cf3a0f5ecd9876061cf361925828908a
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-13 17:03:07 -0600

    added the input for adding personal items

--- src/components/gear/Gear.js
+++ src/components/gear/Gear.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 import { connect } from 'react-redux';
 import './Gear.css';
 
 const Gear = (props) => {
 	const [ gear, setGear ] = useState([]);
 
+	const [ input, setInput ] = useState({});
+
+	const handleChange = (e) => {
+		setInput({ ...input, [e.target.name]: e.target.value });
+	};
+
 	useEffect(() => {
-		Axios.get('/api/gear')
+		axios
+			.get('/api/gear')
 			.then((res) => {
 				setGear(res.data);
 			})
@@ -17,7 +24,8 @@ const Gear = (props) => {
 	}, []);
 
 	const addGear = (gear_id) => {
-		Axios.post(`/api/pack/${props.reducer.selectedPack.backpack_id}`, { gear_id })
+		axios
+			.post(`/api/pack/${props.reducer.selectedPack.backpack_id}`, { gear_id })
 			.then((res) => {
 				setGear(res.data);
 			})
@@ -46,6 +54,51 @@ const Gear = (props) => {
 						</div>
 					);
 				})}
+			</div>
+
+			<div className=" input-fields input-group mx-auto mb-3" style={{ width: '700px' }}>
+				<input
+					type="text"
+					value={gear.name}
+					onChange={handleChange}
+					className="form-control mb-3 mr-5 ml-5"
+					placeholder="name of item"
+				/>
+
+				<div className="input-group mb-3 pr-5 pl-5">
+					<input
+						type="text"
+						value={gear.weight}
+						onChange={handleChange}
+						className="form-control"
+						placeholder="weight of item"
+					/>
+					<div className="input-group-append">
+						<span className="input-group-text" id="basic-addon2">
+							oz
+						</span>
+					</div>
+				</div>
+
+				<div className="input-group pr-5 pl-5">
+					<div className="input-group-prepend">
+						<span className="input-group-text" id="inputGroupFileAddon1">
+							Upload
+						</span>
+					</div>
+					<div className="custom-file">
+						<input
+							type="file"
+							value={gear.image}
+							onChange={handleChange}
+							className="custom-file-input"
+							id="inputGroupFile01"
+						/>
+						<label className="custom-file-label" htmlFor="inputGroupFile01">
+							Choose file
+						</label>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
