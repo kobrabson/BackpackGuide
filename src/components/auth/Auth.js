c911b414190de698c65b475069314a15663d1e16
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-09-13 20:25:23 -0600

    add bootstrap sass

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,95 +1,103 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { loginUser } from '../../redux/reducer';
-
+import './Auth.css';
 
 const Auth = (props) => {
-    const [toggle, setToggle] = useState(true);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    
-    const handleEmailInput = (event) => {
-        const { value } = event.target;
-        setEmail(value);
-    };
-    const handlePasswordInput = (event) => {
-        setPassword(event.target.value);
-    };
-    
-    const login = () => {
-        axios.post("/auth/login", {
-            email: email,
-            password: password,
-        })
-        .then((res) => {
-            props.loginUser(res.data);
-            props.history.push("/backpack");
-        })
-        .catch((err) => {
-            alert("email or password incorrect");
-        });
-    };
-    
-    const register = () => {
-        axios.post("/auth/register", {
-            email: email,
-            password: password,
-        })
-        .then((res) => {
-            props.loginUser(res.data);
-            props.history.push("/backpack");
-        })
-        .catch((err) => {
-            alert("email already registered");
-        });
-    };
-    
-    return (
-        <div>
-        <h1>{toggle ? "Login" : "Register"}</h1>
-        <div>
-            <input
-            name="email"
-            placeholder="email"
-            value={email}
-            onChange={handleEmailInput}
-            />
-            <input
-            name="password"
-            placeholder="password"
-            value={password}
-            onChange={handlePasswordInput}
-            type="password"
-            />
-            {toggle ? (
-            <>
-                <button onClick={login}>Login</button>
-                <button
-                onClick={() => {
-                    setToggle(!toggle);
-                }}
-                >
-                Haven't signed up? Click to register
-                </button>
-            </>
-            ) : (
-            <>
-                <button onClick={register}>Register</button>
-                <button
-                onClick={() => {
-                    setToggle(!toggle);
-                }}
-                >
-                Already signed up? Click to login
-                </button>
-            </>
-            )}
-        </div>
-        </div>
-    );
-    };
+	const [ toggle, setToggle ] = useState(true);
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
 
-    const mapStateToProps = (state) => state;
+	const handleEmailInput = (event) => {
+		const { value } = event.target;
+		setEmail(value);
+	};
+	const handlePasswordInput = (event) => {
+		setPassword(event.target.value);
+	};
+
+	const login = () => {
+		axios
+			.post('/auth/login', {
+				email: email,
+				password: password
+			})
+			.then((res) => {
+				props.loginUser(res.data);
+				props.history.push('/backpack');
+			})
+			.catch((err) => {
+				alert('email or password incorrect');
+			});
+	};
+
+	const register = () => {
+		axios
+			.post('/auth/register', {
+				email: email,
+				password: password
+			})
+			.then((res) => {
+				props.loginUser(res.data);
+				props.history.push('/backpack');
+			})
+			.catch((err) => {
+				alert('email already registered');
+			});
+	};
+
+	return (
+		<div className=" mt-5 mx-auto w-50 card">
+			<h1>{toggle ? 'Login' : 'Register'}</h1>
+			<div className="form-group">
+				<input
+					className="form-control w-75 mx-auto"
+					name="email"
+					placeholder="email"
+					value={email}
+					onChange={handleEmailInput}
+				/>
+				<input
+					className="form-control w-75 mx-auto"
+					name="password"
+					placeholder="password"
+					value={password}
+					onChange={handlePasswordInput}
+					type="password"
+				/>
+				{toggle ? (
+					<div>
+						<button className=" btn-color text-dark btn mt-2 w-25 " type="submit" onClick={login}>
+							Login
+						</button>
+						<br />
+						<button
+							className="btn btn-color btn-dark text-dark mt-2"
+							onClick={() => {
+								setToggle(!toggle);
+							}}
+						>
+							Haven't signed up? Click to register
+						</button>
+					</div>
+				) : (
+					<div>
+						<button onClick={register}>Register</button>
+						<button
+							onClick={() => {
+								setToggle(!toggle);
+							}}
+						>
+							Already signed up? Click to login
+						</button>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
+
+const mapStateToProps = (state) => state;
 
 export default connect(mapStateToProps, { loginUser })(Auth);
